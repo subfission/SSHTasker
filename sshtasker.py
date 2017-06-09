6e590355ae5be0056d6f4b6a573a1f38680d5a67
Zach Jetson <zjetson@asu.edu>
2017-06-08 22:16:26 -0700

    added verbose mode

--- sshtasker.py
+++ sshtasker.py
@@ -30,7 +30,7 @@ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 import sys, os
 import argparse
 from getpass import getpass
-
+from socket import gaierror
 
 class Colors():
     HEADER = '\033[95m'
@@ -50,18 +50,19 @@ try:
 except ImportError as e:
     sys.exit(Colors.RED + "[!] Unable to setup paramiko dependency.  Run: pip3 install paramiko" + Colors.ENDC)
 
-
+file_handle=None
+file_output=[]
+verbose=False
 
 class Connect():
 
-    def __init__(self, user, password, log=False, auto=False, key=False, save=False):
+    def __init__(self, user, password, log=False, auto=False, key=False):
         self.user = user
         self.password = password
         self.log = log
         self.auto = auto
         self.key = key
-        if save:
-            self.file_handle = open(save, 'w')
+
 
         self.client = paramiko.SSHClient()
 
@@ -80,7 +81,7 @@ class Connect():
             if self.auto:
                 self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-            self.client.connect(hostname, port=port, username=self.user, password=self.password, look_for_keys=self.key)
+            self.client.connect(hostname, port=port, username=self.user, password=self.password, look_for_keys=self.key, timeout=3)
             stdin, stdout, stderr = self.client.exec_command(command)
 
             errors = stderr.readlines()
@@ -89,37 +90,38 @@ class Connect():
                 for error in errors:
                     self.exception(str(error), Colors.YELLOW)
 
-            self.output("".join(stdout.readlines()))
-
-            self.client.close()
+            output("".join(stdout.readlines()))
 
         except paramiko.AuthenticationException:
-            self.exception("Authentication failed, please verify your credentials.")
-        except paramiko.SSHException as sshException:
-            self.exception("Unable to establish SSH connection: %s" % sshException)
-        except paramiko.BadHostKeyException as badHostKeyException:
-            self.exception("Unable to verify server's host key: %s" % badHostKeyException)
-        except KeyboardInterrupt:
-            self.exception("Connection terminated by user")
+            exception("Authentication failed, please verify your credentials. %s@%s:%s" % (self.user, hostname, port))
+        except paramiko.SSHException as e:
+            exception("Unable to establish SSH connection: %s:%s %s" % (hostname, port, e))
+        except paramiko.BadHostKeyException as e:
+            exception("Unable to verify server's host key: %s:%s %s" % (hostname, port, e))
+        except gaierror as e:
+            exception("Host not found: %s:%s %s" % (hostname, port, e))
         except Exception as e:
-            self.exception("Operation error: %s" % e)
+            exception("Operation error: %s:%s %s" % (hostname, port, e))
 
-        if self.file_handle:
-            self.file_handle.close()
         self.client.close()
 
 
-    def output(self, msg):
-        print(msg)
-        if self.file_handle:
-            self.file_handle.write(msg)
+
+def output(msg):
+    print(msg)
+    if file_handle:
+        global file_output
+        file_output.append(msg)
 
 
-    def exception(self, msg, color=Colors.RED):
-        msg = '[!] ' + msg
-        print(color + msg + Colors.ENDC)
-        if self.file_handle:
-            self.file_handle.write(msg)
+def exception(msg, color=Colors.RED):
+    msg = '[!] ' + msg
+    print(color + msg + Colors.ENDC)
+    global verbose
+    if verbose and file_handle:
+        global file_output
+        file_output.append(msg)
+
 
 
 def main():
@@ -153,14 +155,19 @@ def main():
     parser.add_argument('--version', action='version', version='%(prog)s 0.8')
     parser.add_argument('--log', action='store_true', help="Enable SSH logging")
     parser.add_argument('--save', metavar="FILE", help="Save output to file")
+    parser.add_argument('--verbose', action='store_true', help="Save output to file")
     args = parser.parse_args()
+
+    if args.verbose:
+        global verbose
+        verbose = True
 
     if not args.key and not args.password:
         password = getpass("Password:")
     else:
         password = args.password
 
-    ssh_tasker = Connect(args.user, password, log=args.log, auto=args.auto, key=args.key, save=args.save)
+    ssh_tasker = Connect(args.user, password, log=args.log, auto=args.auto, key=args.key)
 
     if args.command and os.path.isfile(args.command):
         with open(args.command, 'r') as shell_script:
@@ -168,13 +175,28 @@ def main():
     else:
         command = args.command
 
-    if os.path.isfile(args.list):
-        with open(args.list) as f:
-            for hostname in f:
-                task_hostname(ssh_tasker, hostname, command)
-    else:
-        hostname = args.list
-        task_hostname(ssh_tasker, hostname, command)
+    if args.save:
+        global file_handle
+        file_handle = open(args.save, 'w+')
+
+    try:
+
+        if os.path.isfile(args.list):
+            with open(args.list) as f:
+                for hostname in f:
+                    task_hostname(ssh_tasker, hostname.strip(), command)
+        else:
+            hostname = args.list
+            task_hostname(ssh_tasker, hostname, command)
+    except KeyboardInterrupt:
+        exception("Connection terminated by user")
+        ssh_tasker.client.close()
+
+    if file_handle:
+        global file_output
+        file_handle.writelines(file_output)
+        file_handle.flush()
+        file_handle.close()
 
 def task_hostname(ssh_tasker, hostname, command):
     host = hostname.split(':')
